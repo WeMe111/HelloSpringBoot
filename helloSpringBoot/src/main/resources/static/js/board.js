320fd990b47cc8dc743627ea6e7a1592281d25ad
장우림 <aplm12@naver.com>
2022-05-19 17:11:10 +0900

    test

--- helloSpringBoot/src/main/resources/static/js/board.js
+++ helloSpringBoot/src/main/resources/static/js/board.js
@@ -78,26 +78,6 @@ let index = {
     }
 }
 
-/*
-function uploadFile() {
-  $.ajax({
-    url: "/uploadFile",
-    type: "POST",
-    data: new FormData($("#upload-file-form")[0]),
-    enctype: 'multipart/form-data',
-    processData: false,
-    contentType: false,
-    cache: false,
-    success: function () {
-      // Handle upload success
-      // ...
-    },
-    error: function () {
-      // Handle upload error
-      // ...
-    }
-  });
-}*/
 index.init();
 
 var token = $("meta[name='_csrf']").attr("content");
